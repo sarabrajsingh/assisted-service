49d6e6f005bb155756372b689c4af141428fbd6e
Sarabraj Singh <singh.sarabraj@gmail.com>
2020-11-18 16:16:42 -0500

    added timestamp change (sqlite3 compatible) to cluster.go

--- models/cluster.go
+++ models/cluster.go
@@ -98,14 +98,14 @@ type Cluster struct {
 
 	// The time that this cluster completed installation.
 	// Format: date-time
-	InstallCompletedAt strfmt.DateTime `json:"install_completed_at,omitempty" gorm:"type:timestamp with time zone;default:'2000-01-01 00:00:00z'"`
+	InstallCompletedAt strfmt.DateTime `json:"install_completed_at,omitempty" gorm:"type:timestamp with time zone;default:'2001-01-01T00:00:00.000Z'"`
 
 	// JSON-formatted string containing the user overrides for the install-config.yaml file.
 	InstallConfigOverrides string `json:"install_config_overrides,omitempty" gorm:"type:text"`
 
 	// The time that this cluster started installation.
 	// Format: date-time
-	InstallStartedAt strfmt.DateTime `json:"install_started_at,omitempty" gorm:"type:timestamp with time zone;default:'2000-01-01 00:00:00z'"`
+	InstallStartedAt strfmt.DateTime `json:"install_started_at,omitempty" gorm:"type:timestamp with time zone;default:'2001-01-01T00:00:00.000Z'"`
 
 	// Indicates the type of this object. Will be 'Cluster' if this is a complete object or 'ClusterLink' if it is just a link,
 	// 'AddHostCluster' for cluster that add hosts to existing OCP cluster,
